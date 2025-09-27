bbcca9c5ed227e87720f6fcdae972ffab710fc31
Typezera <willianbardela2131@hotmail.com>
2025-09-27 15:48:17 -0300

    feat: add swagger doc in project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello World s");
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Cadastro de Produtos",
+      version: "1.0.0",
+      description: "Documentation of API Products Register",
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerDoc = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
